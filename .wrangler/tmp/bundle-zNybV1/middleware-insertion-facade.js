c2d721bee4b558f96a854123f99e2629e9e5ab3a
Adem <ademakyasar@gmail.com>
2024-02-13 02:42:20 -0500

    added python notebooks to tokenize stickers

--- .wrangler/tmp/bundle-zNybV1/middleware-insertion-facade.js
+++
@@ -1,3 +0,0 @@
-import { __facade_registerInternal__ } from "E:\\DOCUMENTS\\sticker-compose\\sticker-composer\\node_modules\\wrangler\\templates\\middleware\\loader-sw.ts";
-import * as __MIDDLEWARE_0__ from "E:\\DOCUMENTS\\sticker-compose\\sticker-composer\\node_modules\\wrangler\\templates\\middleware\\middleware-miniflare3-json-error.ts";
-__facade_registerInternal__([__MIDDLEWARE_0__.default])
