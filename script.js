2e9591bb5a33e13305d68859ec9742da0c13c361
Radoš Milićev <40705899+rammba@users.noreply.github.com>
2024-02-14 16:46:56 +0100

    Clear info message result is correct

--- script.js
+++ script.js
@@ -85,6 +85,7 @@ function populateResults(resultIndex = 0) {
       selectedStickersList.style.display = "none"
   } else {
       selectedStickersList.style.display = "block"
+      clearInfoMessage()
   }
   renderSelectedStickers(selectedStickers)
 }
